b2e4cb89b48b8801a5dbef3ba244ee4bd60fa62e
Patrick Amrein <amrein@ubique.ch>
2025-09-18 12:03:41 +0200

    add license header

--- src/jwt/jwk.rs
+++ src/jwt/jwk.rs
@@ -1,9 +1,19 @@
-// use base64::{Engine, prelude::BASE64_STANDARD};
-// use josekit::{jwk::Jwk, jws::JwsVerifier};
-// use x509_cert::der::{
-//     Decode, DecodePem,
-//     oid::db::rfc5912::{ID_EC_PUBLIC_KEY, RSA_ENCRYPTION},
-// };
+/* Copyright 2025 Ubique Innovation AG
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 
 // use crate::models::errors::{FederationError, X509Error};
 
